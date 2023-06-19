f1600f13cddaf093b3f5739050705cb4459e493c
lucasrls98 <lucasrls98@gmail.com>
2023-06-19 18:03:35 -0300

    feat: usando while para acessar a lista de sons

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-document.querySelector(".tecla_pom")
-document.querySelector("#som_tecla_pom").play()
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla")
+
+let contador = 0;
+
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador]
+
+    const instrumento = tecla.classList[1]
+
+    // template string
+
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+
+    contador = contador + 1
+
+    // console.log(contador)
+}
+
+
+
