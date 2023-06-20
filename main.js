4609007bf216a6ef3074285eddea07e618068940
lucasrls98 <lucasrls98@gmail.com>
2023-06-20 13:56:04 -0300

    for looping

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listaDeTeclas = document.querySelectorAll(".tecla")
 let contador = 0;
 
 
-while(contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
 
     const tecla = listaDeTeclas[contador]
 
@@ -22,7 +22,7 @@ while(contador < listaDeTeclas.length){
         tocaSom(idAudio)
     }
 
-    contador = contador + 1
+    
 
     // console.log(contador)
 }
